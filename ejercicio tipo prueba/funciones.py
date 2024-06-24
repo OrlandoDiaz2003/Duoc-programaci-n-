a30e48cbd5b042959509f9f67e5cd207ef122224
Orlando <or.diaz@duocuc.cl>
2024-06-24 12:21:55 -0400

    a

--- ejercicio tipo prueba/funciones.py
+++ ejercicio tipo prueba/funciones.py
@@ -25,6 +25,19 @@ def confirmar_boleto(boletos,correos):
         correo_buscar = input("Ingrese el correo a buscar: ").lower()
         if info['correo']==correo_buscar:
             print(f"Boleto encontrado \n Nombre: {info['nombre'].title()} \n Estado: {info['estado']}")
+            while True:
+                rsp=input("Deseas confirmar boleto(s/n): ").lower()
+                if rsp=="s":
+                    info['estado']="confirmado"
+                    print("Confirmacion completada")
+                    break
+                elif rsp=="n":
+                    print("No se ha cambiado el estado")
+                    break
+                else:
+                    print("Ingrese respuesta valida")
+                    continue
+        break
 
             
         #print(posicion_boleto)
